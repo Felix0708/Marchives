f6bba120cd4c10686618ddcba98b941e9c704f3d
FELIX0708 <chuls0708@gmail.com>
2022-02-20 09:14:07 +0900

    react practice

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSample from './InputSample';
 import './App.css';
 
 
@@ -14,13 +17,20 @@ function App() {
 
   return (
     <>
-      {/* 주석은 화면에 보이지 않습니다 */}
-      /* 중괄호로 감싸지 않으면 화면에 보입니다 */
-      <Hello 
-        // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
-      />
+      <Wrapper>
+        {/* 주석은 화면에 보이지 않습니다 */}
+        /* 중괄호로 감싸지 않으면 화면에 보입니다 */
+        <Hello 
+          // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
+          name="react" color="red" isSpecial
+        />
+        {/* isSpecial={true} */}
+        <Hello color="pink"/>
+      </Wrapper>
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
+      <Counter />
+      <InputSample />
     </>
   );
 }
